9b5d1bdbc80cf86822136ec0ad848fd0182fb113
Rayan <rayanalim@users.noreply.github.com>
2021-06-11 22:48:07 -0400

    started on visualisation for interactive maps

--- VIsualisation/interactivemap.py
+++ VIsualisation/interactivemap.py
@@ -1,19 +1,19 @@
 import pandas as pd
+import geopandas as gdp
 import plotly
 import json
 
-
-df = pd.read_csv("/Users/alim/Documents/GitHub/Team-4-AI-4-Good-lab/datasets/schools-in-nigeria/raw-files/raw-csv-files/Religious-schools.csv",
-                   dtype={"fips": str})
-                   
-dff = gdp.read_file("private-schools.geojson")
-
 import plotly.express as px
 
-fig = px.choropleth(df, geojson=dff, locations='geometry', color='no_of_students',
-                           color_continuous_scale="no_of_teachers",
-                           range_color=(0, 12),
-                           scope="africa"
+df = pd.read_csv('/Users/alim/Documents/GitHub/Team-4-AI-4-Good-lab/datasets/Schools-with-lat-long.csv')
+df_wb = pd.read_csv("/Users/alim/Documents/GitHub/Team-4-AI-4-Good-lab/datasets/world-bank-data/cons_agg_wave3_visit2.csv")
+dff = gdp.read_file("/Users/alim/Documents/GitHub/Team-4-AI-4-Good-lab/datasets/nigeria_geojson.geojson")
+geo_df = gdp.read_file("/Users/alim/Documents/GitHub/Team-4-AI-4-Good-lab/datasets/nigeria_geojson.geojson")
+
+fig = px.choropleth_mapbox(df, geojson=geo_df.geometry, locations='geometry', color='lga',
+                           mapbox_style="carto-positron",
+                           zoom=4
                           )
+
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
